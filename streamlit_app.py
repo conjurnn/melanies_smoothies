ec4b0762163dd459f3db289e05737bfd01b784f6
Ryan <ryanhancock2002@gmail.com>
2026-09-11 13:07:09 +0000

    IT IS NOT EMPYT

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 # convert snowpark datframe ito a panda dataframe
 pd_df=my_dataframe.to_pandas()
-# st.dataframe(pd_df)
+st.dataframe(pd_df)
 # st.stop()
 
 ingredients_list = st.multiselect(
@@ -40,7 +40,7 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
 
         st.subheader(fruit_chosen + ' Nutriton Information')
